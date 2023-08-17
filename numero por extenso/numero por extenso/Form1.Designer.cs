afb49bac96507130570ab28ccb1c53edf35250cb
Vinícius Tavares Mota <106537834+viniciustmota@users.noreply.github.com>
2023-08-17 18:04:23 -0300

    Add files via upload

---
+++ numero por extenso/numero por extenso/Form1.Designer.cs
@@ -0,0 +1,152 @@
+﻿namespace numero_por_extenso
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.cboNumeros = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtNumeroExtenso = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(80, 48);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(265, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "SELECIONE UM NÚMERO:";
+            // 
+            // cboNumeros
+            // 
+            this.cboNumeros.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cboNumeros.FormattingEnabled = true;
+            this.cboNumeros.Items.AddRange(new object[] {
+            "0",
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7",
+            "8",
+            "9",
+            "10"});
+            this.cboNumeros.Location = new System.Drawing.Point(360, 45);
+            this.cboNumeros.Name = "cboNumeros";
+            this.cboNumeros.Size = new System.Drawing.Size(65, 32);
+            this.cboNumeros.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(80, 204);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(255, 24);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "NÚMERO POR EXTENSO";
+            // 
+            // txtNumeroExtenso
+            // 
+            this.txtNumeroExtenso.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNumeroExtenso.Location = new System.Drawing.Point(84, 241);
+            this.txtNumeroExtenso.Name = "txtNumeroExtenso";
+            this.txtNumeroExtenso.Size = new System.Drawing.Size(251, 29);
+            this.txtNumeroExtenso.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(84, 335);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(120, 40);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "LIMPAR";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(361, 335);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(120, 40);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "FECHAR";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(126, 99);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(146, 61);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "GERAR EXTENSO";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.Button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtNumeroExtenso);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.cboNumeros);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cboNumeros;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtNumeroExtenso;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ numero por extenso/numero por extenso/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace numero_por_extenso
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            DialogResult mensagem = MessageBox.Show("Deseja finalizar a aplicação?", "Encerrar", MessageBoxButtons.YesNo, MessageBoxIcon.Question, MessageBoxDefaultButton.Button2); if (mensagem == DialogResult.Yes) { this.Close(); }
+        }
+
+        private void Button3_Click(object sender, EventArgs e)
+        {
+            switch (cboNumeros.Text)
+            {
+                case "0":
+                    txtNumeroExtenso.Text = "Zero";
+                    break;
+                case "1":
+                    txtNumeroExtenso.Text = "Um";
+                    break;
+                case "2":
+                    txtNumeroExtenso.Text = "Dois";
+                    break;
+                case "3":
+                    txtNumeroExtenso.Text = "Tres";
+                    break;
+                case "4":
+                    txtNumeroExtenso.Text = "Quatro";
+                    break;
+                case "5":
+                    txtNumeroExtenso.Text = "Cinco";
+                    break;
+                case "6":
+                    txtNumeroExtenso.Text = "Seis";
+                    break;
+                case "7":
+                    txtNumeroExtenso.Text = "Sete";
+                    break;
+                case "8":
+                    txtNumeroExtenso.Text = "Oito";
+                    break;
+                case "9":
+                    txtNumeroExtenso.Text = "Nove";
+                    break;
+                case "10":
+                    txtNumeroExtenso.Text = "Dez";
+                    break;
+            }
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            cboNumeros.Text = "";
+            txtNumeroExtenso.Text = "";
+        }
+    }
+}
